6a4bdeaecbaa9ff87b6c5334cca1f45cc78ede3b
MelikaZ <uchiha.7990@gmail.com>
2022-12-02 20:23:15 +0330

    phase 2 part 1: clean up

--- main.py
+++ main.py
@@ -2,388 +2,119 @@ import pandas as pd
 from datetime import datetime
 from prettytable import PrettyTable
 
-dataSet1 = pd.read_csv('DataSets/INOUT.csv')
-dataSet2 = pd.read_csv('DataSets/INOUTLINE.csv')
-dataSet3 = pd.read_csv('DataSets/TRANSFER_ITEM_D.csv')
-dataSet4 = pd.read_csv('DataSets/TRANSFER_ITEM.csv')
-dataSet5 = pd.read_csv('DataSets/PRODUCTS.csv')
-dataSet6 = pd.read_csv('DataSets/PRODUCTINSTANCE.csv', encoding='cp1252')
+dataSet1 = pd.read_csv('Data set/INOUT.csv')
+dataSet2 = pd.read_csv('Data set/INOUTLINE.csv')
+dataSet3 = pd.read_csv('Data set/PRODUCTINSTANCE.csv', encoding='cp1252')
+dataSet4 = pd.read_csv('Data set/PRODUCTS.csv')
+dataSet5 = pd.read_csv('Data set/TRANSFER_ITEM.csv')
+dataSet6 = pd.read_csv('Data set/TRANSFER_ITEM_D.csv')
 dataSets = [dataSet1, dataSet2, dataSet3, dataSet4, dataSet5, dataSet6]
+
 
 def dateValidityCheck(data):
     validCount = 0
     for i in range(dataSize):
-        try:
-            datetime.strptime(data[i], "%m/%d/%Y %H:%M")
-            validCount += 1
-        except Exception:
-            continue
-    return (validCount/dataSize)*100
+        for fmt in ("%m/%d/%Y %H:%M","%m/%d/%Y %H:%M:%S %p"):
+            try:
+                datetime.strptime(data[i], fmt)
+                validCount += 1
+            except Exception:
+                continue
+    return round((validCount/dataSize)*100, 3)
+
 
 def stringToInt(data):
     df = pd.to_numeric(data, errors='coerce')  # string ha tabdil be null mishan
     return floatToInt(df)
 
+
 def floatToInt(data):
     return data.convert_dtypes()
 
+
 def validityCheck(data):
     validData = data.dropna().size
-    return (validData/dataSize)*100
+    return round((validData/dataSize)*100, 3)
 
-# 111111111111111111111
+
 tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
 
-stringToIntData = ["SUB_REF_ORG","RESPONSIBLEBPARTNER","C_DOCTYPE_ID","DESCRIPTION"]
-
-floatToIntData = ["DOCUMENTNO","REF_ORG","C_COSTCENTER_ID","COM_BPARTNER_ID","M_PRODUCT_ID","REQUESTNO",\
+floatToIntData1 = ["DOCUMENTNO","REF_ORG","C_COSTCENTER_ID","COM_BPARTNER_ID","M_PRODUCT_ID","REQUESTNO",\
                   "RESPONSIBLEMANAGER","FROMUSERMANAGERID","COM_BPARTNER_ID_F","M_INOUT_AMVAL_ID","DOCUMENTNO1",\
                   "LOCATIONS_ID","ACCT_AC_HOLDING_ID","LETTERNO","VAHED_MALI","ACCT_AC_JOURNAL_ID","BASEINFO_RECORDID","C_YEAR_ID"]
+floatToIntData2 = ["VAHED_MALI","PRODUCT_BATCH_ID","BASEINFO_RECORDID","C_YEAR_ID","M_INOUT_AMVAL_ID",]
+floatToIntData3 = ["M_PRODUCT_ID","UPDATED","UPDATEDBY","","DATE1","PARENT_ID","ISSUMMARY","BOOKVALUE"]
+floatToIntData4 = []
+floatToIntData5 = []
+floatToIntData6 = []
 
-dateData = ["LETTERDATE","REQUESTDATE","DOCUMENTDATE","UPDATED","CREATED"]
+dateData1 = ["LETTERDATE","REQUESTDATE","DOCUMENTDATE","UPDATED","CREATED"]
+dateData2 = ["OPERATIONDATE","REACHMENTDATE","UPDATED","CREATED"]
+dateData3 = ["REACHMENTDATE","DATE1","UPDATED","CREATED","ENDOFUSEFULLIFE","OPERATIONDATE","DOCUMENTDATE","LETTERDATE"]
+dateData4 = ["CREATED","UPDATED"]
+dateData5 = ["CREATED","UPDATED","OPERATIONDATE","FROM_DATE","LETTER_DATE"]
+dateData6 = ["CREATED","UPDATED","REACHMENTDATE","OPERATIONDATE","TRANSFER_ITEM_ID"]
 
+stringToIntData1 = ["SUB_REF_ORG","RESPONSIBLEBPARTNER","C_DOCTYPE_ID","DESCRIPTION"]
+stringToIntData2 = []
+stringToIntData3 = ["AD_CLIENT_ID","AD_ORG_ID","C_COSTCENTER_ID","AD_ORG_REF_ID","M_PI_STATUS_ID","HASCONTROL",\
+                    "M_PRODUCTINSTANCE_BASEINFO_ID","COSTLINE","PRIMALVALUE","USEFULLIFE","SALVAGEVALUE",\
+                    "PI_VALUEAFTERCOEFFICIENTINC","OPERATIONDATE","PRESENTVALUE","COM_BPARTNER_ID","M_INOUT_AMVAL_ID",\
+                    "DOCUMENNO","SUB_REF_ORG","DEPRECATIONRATE","RETURNAMVALTOANBAR","TYPEMELKOFCONTRACT","NEW_L_DATE","MAP_DATE"]
+stringToIntData4 = ["C_UOM_ID","NEXTLEVELDIGITS","CODEDIGITS","IRANCODE","IRANCODE","DESC1","M_ACCOUNTINGASSET_ID","M_PRODUCT_ID2",\
+                    "M_PRODUCT_TYPE_ID","ESTEHLAK_GROUP_CODE","HAZINE_GROUP","SATH","OLGO_M_PRODUCT_ID"]
+stringToIntData5 = ["OLDLABELNO","M_TRANSFER_KALA_ID","DOC_NO","DOCUMENTNO","ADDRESS","LOCATIONS_ID","VAHED_MALI","TYPEPROPERTY"]
+stringToIntData6 = ["RETURNAMVALTOANBAR","TYPEPROPERTY","C_YEAR_ID","VAHED_MALI","TYPE_PRODUCT"]
 
-nullCount = []
-completeness = []
-validity = []
-attributeName = []
-sizeList = []
-
-table = PrettyTable()
-for attribute in dataSet1.columns:
-    attributeName.append(attribute)
-    data = dataSet1[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
-
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet1[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet1[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
-        validity.append(result)
-        continue
-
-    result = validityCheck(data)
-    validity.append(result)
-
-
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
-
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
-
-
-# 222222222222222222222
-tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
-
-stringToIntData = []
-
-floatToIntData = ["VAHED_MALI","PRODUCT_BATCH_ID","BASEINFO_RECORDID","C_YEAR_ID","M_INOUT_AMVAL_ID",]
-
-dateData = ["OPERATIONDATE","REACHMENTDATE","UPDATED","CREATED"]
+dateDataList = [dateData1, dateData2, dateData3, dateData4, dateData5, dateData6]
+floatToIntDataList = [floatToIntData1, floatToIntData2, floatToIntData3, floatToIntData4, floatToIntData5, floatToIntData6]
+stringToIntDataList = [stringToIntData1, stringToIntData2, stringToIntData3, stringToIntData4, stringToIntData5, stringToIntData6]
 
 nullCount = []
 completeness = []
 validity = []
 attributeName = []
 sizeList = []
-
 table = PrettyTable()
-for attribute in dataSet2.columns:
-    attributeName.append(attribute)
-    data = dataSet2[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
 
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet2[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet2[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
+for dataSet in dataSets:
+    dateData = dateDataList.pop(0)
+    stringToIntData = stringToIntDataList.pop(0)
+    floatToIntData = floatToIntDataList.pop(0)
+
+    for attribute in dataSet.columns:
+        attributeName.append(attribute)
+        data = dataSet[attribute]
+        dataSize = data.size
+        sizeList.append(dataSize)
+        nonNullDataSize = data.dropna().size
+        completenessValue = round((nonNullDataSize/dataSize)*100, 3)
+        nullCount.append(dataSize - nonNullDataSize)
+        completeness.append(completenessValue)
+
+        if attribute in stringToIntData:
+            data = stringToInt(data)
+        elif attribute in floatToIntData:
+            data = floatToInt(data)
+        elif attribute in dateData:
+            result = dateValidityCheck(data)
+            validity.append(result)
+            continue
+
+        result = validityCheck(data)
         validity.append(result)
-        continue
 
-    result = validityCheck(data)
-    validity.append(result)
+    table.add_column(tableTitle[0], attributeName)
+    table.add_column(tableTitle[1], sizeList)
+    table.add_column(tableTitle[2], nullCount)
+    table.add_column(tableTitle[3], completeness)
+    table.add_column(tableTitle[4], validity)
 
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
+    print(table)
 
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
-
-# 333333333333333333333
-tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
-
-stringToIntData = ["AD_CLIENT_ID","AD_ORG_ID","C_COSTCENTER_ID","AD_ORG_REF_ID","M_PI_STATUS_ID","HASCONTROL","M_PRODUCTINSTANCE_BASEINFO_ID",\
-    "COSTLINE","PRIMALVALUE","USEFULLIFE","SALVAGEVALUE","PI_VALUEAFTERCOEFFICIENTINC","OPERATIONDATE","PRESENTVALUE",\
-        "COM_BPARTNER_ID","M_INOUT_AMVAL_ID","DOCUMENNO","SUB_REF_ORG","DEPRECATIONRATE","RETURNAMVALTOANBAR","TYPEMELKOFCONTRACT",\
-            "NEW_L_DATE","MAP_DATE"]
-
-floatToIntData = ["M_PRODUCT_ID","UPDATED","UPDATEDBY","","DATE1","PARENT_ID","ISSUMMARY","BOOKVALUE"]
-
-dateData = ["REACHMENTDATE","DATE1","UPDATED","CREATED","ENDOFUSEFULLIFE","OPERATIONDATE","DOCUMENTDATE","LETTERDATE"]
-
-nullCount = []
-completeness = []
-validity = []
-attributeName = []
-sizeList = []
-
-table = PrettyTable()
-for attribute in dataSet2.columns:
-    attributeName.append(attribute)
-    data = dataSet2[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
-
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet2[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet2[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
-        validity.append(result)
-        continue
-
-    result = validityCheck(data)
-    validity.append(result)
-
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
-
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
-
-
-# 444444444444444444444
-tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
-
-stringToIntData = ["C_UOM_ID","NEXTLEVELDIGITS","CODEDIGITS","IRANCODE","IRANCODE","DESC1","M_ACCOUNTINGASSET_ID","M_PRODUCT_ID2",\
-    "M_PRODUCT_TYPE_ID","ESTEHLAK_GROUP_CODE","HAZINE_GROUP","SATH","OLGO_M_PRODUCT_ID"]
-
-floatToIntData = []
-
-dateData = ["CREATED","CREATED"]
-
-nullCount = []
-completeness = []
-validity = []
-attributeName = []
-sizeList = []
-
-table = PrettyTable()
-for attribute in dataSet2.columns:
-    attributeName.append(attribute)
-    data = dataSet2[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
-
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet2[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet2[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
-        validity.append(result)
-        continue
-
-    result = validityCheck(data)
-    validity.append(result)
-
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
-
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
-# 555555555555555555555
-tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
-
-stringToIntData = ["OLDLABELNO","M_TRANSFER_KALA_ID","DOC_NO","DOCUMENTNO","ADDRESS","LETTER_DATE","LOCATIONS_ID","VAHED_MALI",\
-    "TYPEPROPERTY"]
-
-floatToIntData = []
-
-dateData = ["CREATED","UPDATED","OPERATIONDATE","FROM_DATE",""]
-
-nullCount = []
-completeness = []
-validity = []
-attributeName = []
-sizeList = []
-
-table = PrettyTable()
-for attribute in dataSet2.columns:
-    attributeName.append(attribute)
-    data = dataSet2[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
-
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet2[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet2[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
-        validity.append(result)
-        continue
-
-    result = validityCheck(data)
-    validity.append(result)
-
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
-
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
-# 666666666666666666666
-tableTitle = ["name", "data size", "null count", "Completeness", "Validity"]
-
-stringToIntData = ["RETURNAMVALTOANBAR","TYPEPROPERTY","C_YEAR_ID","VAHED_MALI","TYPE_PRODUCT"]
-
-floatToIntData = []
-
-dateData = ["CREATED","UPDATED","REACHMENTDATE","OPERATIONDATE","TRANSFER_ITEM_ID",""]
-
-nullCount = []
-completeness = []
-validity = []
-attributeName = []
-sizeList = []
-
-table = PrettyTable()
-for attribute in dataSet2.columns:
-    attributeName.append(attribute)
-    data = dataSet2[attribute]
-    dataSize = data.size
-    sizeList.append(dataSize)
-    nonNullDataSize = data.dropna().size
-    completenessValue = (nonNullDataSize/dataSize)*100
-    nullCount.append(dataSize - nonNullDataSize)
-    completeness.append(completenessValue)
-
-    if attribute in stringToIntData:
-        data = stringToInt(dataSet2[attribute])
-    elif attribute in floatToIntData:
-        data = floatToInt(dataSet2[attribute])
-    elif attribute in dateData:
-        result = dateValidityCheck(data)
-        validity.append(result)
-        continue
-
-    result = validityCheck(data)
-    validity.append(result)
-
-table.add_column(tableTitle[0], attributeName)
-table.add_column(tableTitle[1], sizeList)
-table.add_column(tableTitle[2], nullCount)
-table.add_column(tableTitle[3], completeness)
-table.add_column(tableTitle[4], validity)
-
-print(table)
-
-table.clear()
-tableTitle.clear()
-stringToIntData.clear()
-floatToIntData.clear()
-dateData.clear()
-nullCount.clear()
-completeness.clear()
-validity.clear()
-attributeName.clear()
-sizeList.clear()
-
+    table.clear()
+    nullCount.clear()
+    completeness.clear()
+    validity.clear()
+    attributeName.clear()
+    sizeList.clear()
