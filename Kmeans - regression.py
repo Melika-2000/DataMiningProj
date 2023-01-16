6c85e1ab86f09f793077a783b34751967df062a8
seymota <seyed.m.t.t@gmail.com>
2023-01-16 21:36:38 +0330

    images updated

--- Kmeans - regression.py
+++ Kmeans - regression.py
@@ -14,16 +14,19 @@ plt.style.use(plt.style.available[5])
 import warnings
 warnings.filterwarnings("ignore")
 
-# part 3
+
 # Loading Data
 products_DataSet = pd.read_csv('Data set/PRODUCTS.csv')
 
 productInstance_DataSet = pd.read_csv('Data set/PRODUCTINSTANCE.csv', encoding='cp1252')
 
-productInstance = productInstance_DataSet[['M_PRODUCT_ID',"BOOKVALUE"]].copy()
+productInstance = productInstance_DataSet[['M_PRODUCT_ID',"BOOKVALUE","PRIMALVALUE"]].copy()
 products = products_DataSet[['M_PRODUCT_ID','NAME',"VALUE"]].copy()
 
-# Pre-proccessing
+
+"""
+part 1: # Pre-proccessing
+"""
 print("*****************running*******************")
 ls = []
 for i,record in enumerate(products['NAME']):
@@ -67,9 +70,9 @@ products['VALUE'] = products['VALUE'].astype('float64')
 merged = pd.merge(left=products,right=productInstance,how='inner',left_on='M_PRODUCT_ID',right_on='M_PRODUCT_ID')
 
 # remove null records
-cleanData = merged[['NAME',"VALUE",'BOOKVALUE']].dropna()
+cleanData = merged[['NAME',"VALUE",'BOOKVALUE','PRIMALVALUE']].dropna()
 
-Xc = cleanData[['VALUE','BOOKVALUE']].copy()
+Xc = cleanData[['VALUE','BOOKVALUE','PRIMALVALUE']].copy()
 
 # cast data to numpy, for easier use in libraries
 X = Xc.to_numpy()
@@ -84,11 +87,14 @@ train = standard.transform(X_train_np)
 test = standard.transform(X_test_np)
 
 
+"""
+part 2: clustering model
+"""
 # Instantiate the clustering model
 model = KMeans()
 
 # elbow method to find best "k" for K-means clustring
-# n_clusters = elbow(model,(30,50),train)
+# n_clusters = elbow(model,(30,70),train)
 # print(n_clusters)
 
 # 48 is a good practical 'k', obtained by running elbow method on train data
@@ -96,6 +102,7 @@ model.n_clusters = 48
 
 # fit K-means method
 kmeans = model.fit(train)
+train_labels = kmeans.labels_
 
 figure = plt.figure(figsize=(9,7))
 plt.title("Train Result")
@@ -124,9 +131,56 @@ plt.scatter(test[:, 0], test[:, 1], c=predictions,s=180)
 plt.title("Test Predictions")
 plt.show()
 
-print(f"Silhouette Coefficient (train): {metrics.silhouette_score(train, kmeans.labels_):.3f}")
+print(f"Silhouette Coefficient (train): {metrics.silhouette_score(train, train_labels):.3f}")
 print(f"Silhouette Coefficient (test): {metrics.silhouette_score(test, predictions):.3f}")
 
 plt.scatter(test[index, 0], test[index, 1], c=predictions[index],s=250)
 plt.title("Selected Points")
 plt.show()
+
+
+"""
+part 3: regression model for price estimation
+"""
+# select a random record from test data
+maxBoundary = len(test)
+test_record_ind = random_int(0,maxBoundary)
+# check label
+TRI_lbl = predictions[test_record_ind]
+
+# generate the cluster which test record belongs to, from train data.
+cluster_index = []
+for i,lbl in enumerate(train_labels):
+    if lbl == TRI_lbl:
+        cluster_index.append(i)
+print(len(cluster_index))
+
+# select regression parameters
+x1 = X_train_np[cluster_index,1]
+y1 = X_train_np[cluster_index,2]
+
+# plot cluster
+plt.axline((0, 0), slope=1, color="red", linestyle=(0, (5, 5)))
+plt.xlabel('PRIMALVALUE')
+plt.ylabel('BOOKVALUE')
+plt.scatter(x1,y1)
+ax = plt.gca()
+ax.grid(True)
+plt.show()
+
+
+from sklearn import linear_model
+
+x1r = x1.reshape((-1,1))
+
+# regression model
+regressor = linear_model.LinearRegression(positive=True)
+regressor.fit(x1r,y1)
+print("Coefficients: \n    ", regressor.coef_)
+
+
+# estimate price for selected test data
+x2 = X_test_np[test_record_ind,1]
+y2 = regressor.predict(np.reshape(x2,(1,-1)))
+
+print("the predicted price is: ",y2)
